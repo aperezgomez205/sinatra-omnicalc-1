a5379b2ba69129ce0a1cb8c43f4ad72c222bbc65
aperezgomez205 <143421182+aperezgomez205@users.noreply.github.com>
2024-02-29 16:32:42 +0000

    works

--- app.rb
+++ app.rb
@@ -2,8 +2,47 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/square/new") do
+  erb(:new_sq_calc)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2.0
+  erb(:sq_results)
+end
+
+get("/square_root/new") do
+  erb(:new_sqrt_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 0.5
+  erb(:sqrt_results)
+end
+
+get ("/payment/new") do
+  erb(:new_payment_calc)
+end
+
+get ("/payment/results") do
+  @apr = params.fetch("apr").to_f
+  @yrs_remain = params.fetch("yrs_remain").to_f
+  @present_value = params.fetch("present_value").to_f
+
+  @r = @apr / 12.0
+  @n = @yrs_remain * 12.0
+
+  @numerator =  (@r * @present_value)
+  @denom = (1.0 - ((1.0 + @r)**-@n))
+
+  @the_result = @numerator / @denom
+
+  erb(:payment_results)
 end
